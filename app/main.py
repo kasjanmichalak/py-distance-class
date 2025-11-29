5f0875b6cfcee19b091a1771943c03dd4703c666
Kasjan Michalak <kasjan.michalak@gmail.com>
2025-11-29 21:18:21 +0000

    3rd save

--- app/main.py
+++ app/main.py
@@ -6,42 +6,37 @@ class Distance:
         return f"Distance: {self.km} kilometers."
 
     def __repr__(self) -> str:
-        return f"Distance (km={self.km}"
+        return f"Distance (km={self.km})"
 
-    def __add__(self, other: float) -> float:
+    def __add__(self, other: float) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         else:
             return Distance(self.km + other)
 
-    def  __iadd__(self, other: float) -> float:
+    def  __iadd__(self, other: float) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
         else:
             self.km += other
         return self
 
-    def __mul__(self, other: float) -> float:
-        if isinstance(other, Distance):
-            return Distance(self.km * other.km)
-        else:
-            return Distance(self.km * other)
+    def __mul__(self, other: float) -> Distance:
+       return Distance(self.km * other)
 
-    def __truediv__(self, other: float) -> float:
+    def __truediv__(self, other: float) -> Distance:
         if other == 0:
             raise ValueError("Division by zero")
-        if isinstance(other, Distance):
-            return Distance(round((self.km / other.km), 2))
         else:
             return Distance(round((self.km / other), 2))
 
-    def __lt__(self, other: float) -> float:
+    def __lt__(self, other: float) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
         else:
             return self.km < other
 
-    def __gt__(self, other: float) -> float:
+    def __gt__(self, other: float) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
         else:
